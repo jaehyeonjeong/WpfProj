dc98987fb5c84c312b77dd12fb5c6153ffc3c760
home\UserFirst <definejae234@gmail.com>
2026-08-27 14:31:06 +0900

    DepthConverter (계층 표현) 까지 구현

---
+++ WpfExplorer/WpfExplorer.Main/Local/ViewModels/MainContentViewModel.cs
@@ -0,0 +1,28 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using Jamesnet.Wpf.Mvvm;
+using System.Collections.Generic;
+using WpfExplorer.Support.Local.Helpers;
+using WpfExplorer.Support.Local.Models;
+
+namespace WpfExplorer.Main.Local.ViewModels
+{
+    public partial class MainContentViewModel : ObservableBase
+    {
+        private readonly FileService _fileService;
+
+        public List<FolderInfo> Roots { get; init; }
+
+        public MainContentViewModel(FileService fileService)
+        {
+            _fileService = fileService;
+
+            Roots = _fileService.GenerateRootNodes();
+        }
+
+        [RelayCommand]
+        private void FolderChanged(FolderInfo folderInfo)
+        {
+            _fileService.RefreshSubdirectories(folderInfo);
+        }
+    }
+}
