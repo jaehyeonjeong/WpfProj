0dcb180d533932afc9497c337989e06110506e9c
home\UserFirst <definejae234@gmail.com>
2026-09-02 16:29:38 +0900

    Chapter3 - PickupCard 콘솔 방식

---
+++ HeadFirst/PickRandomCards/PickRandomCards/Program.cs
@@ -0,0 +1,17 @@
+﻿using PickRandomCards;
+
+Console.WriteLine("Enter the number of cards to pick : ");
+
+string line = Console.ReadLine();
+
+if(int.TryParse(line, out int numberOfCards))       //  string 타입의 line 변수를 int 타입의 numberOfCards에 저장
+{
+    foreach(string card in CardPicker.PickSomeCards(numberOfCards))
+    {
+        Console.WriteLine(card);
+    }
+}
+else
+{
+    Console.WriteLine($"Please input the Card Data.");
+}
